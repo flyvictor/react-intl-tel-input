48e9a88f58beb43d5563d6289cf2cfda100b6408
alex.gola <alex.gola93@gmail.com>
2019-02-18 15:42:06 +0100

    PO-492: scripts

--- scripts/build.js
+++ scripts/build.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 /* eslint-disable import/no-extraneous-dependencies,import/no-dynamic-require,global-require */
 // Do this as the first thing so that any code reading it knows the right env.
 process.env.NODE_ENV = 'production';
